7fdd28d4bcc4c6f0833e63e00989355581ccc924
Suman Kisku <sumankisku1@gmail.com>
2023-01-15 23:19:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function threeSum(arr, target) {
   //your code here
+	arr.sort();
+	const n = arr.length;
+	let closestSum = Number.MAX_VALUE;
+	for(let i = 0; i < n - 2; i++) {
+		let ptr1 = i + 1;
+		let ptr2 = n - 1;
+		while(ptr1<ptr2) {
+			let sum = arr[i] + arr[ptr1] + arr[ptr2];
+			if(Math.abs(x - sum) < abs(x - closestSum)) {
+				closestSum = sum;
+			}
+			if(sum > x) {
+				ptr2--;
+			} else {
+				ptr1++;
+			}
+		}
+	}
+	console.log(closestSum);
 }
 
 module.exports = threeSum;
